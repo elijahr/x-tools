12eaacdf6bdd8d96674e50ddcfa1fedbdf1911ba
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-17 00:11:59 -0600

    Missing slash

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 
 generate_image_name () {
   sed=$(which gsed || which sed) # needs gnu-sed
-  tag="$1--$2--$(echo $3 | $sed -r 's/linux\///')"
+  tag="$1-$2-$(echo $3 | $sed -r 's/linux\///')"
   tag=$(echo $tag | $sed -r 's/[^a-zA-Z0-9.]+/-/g' | $sed -r 's/^-+\|-+$//g')
   echo "elijahr/crosstools-ng:$tag"
 }
